3105fd3208bd8d799441cfd819d05bbcf7988ca2
user180d <mailuser180@gmail.com>
2025-06-18 23:48:49 +0300

    End of phase 12

--- API/Program.cs
+++ API/Program.cs
@@ -1,8 +1,10 @@
 using API.Middleware;
 using Core.Interfaces;
 using Infrastructure.Data;
+using Infrastructure.Services;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.EntityFrameworkCore;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -16,6 +18,17 @@ builder.Services.AddDbContext<StoreContext>(opt =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 builder.Services.AddCors();
+
+//Connect to reddis
+builder.Services.AddSingleton<IConnectionMultiplexer>(config =>
+{
+    var connString = builder.Configuration.GetConnectionString("Redis")
+        ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
+//add singleton service for redis
+builder.Services.AddSingleton<ICartService, CartService>();
 
 var app = builder.Build();
 
---
+++ Core/Entities/ShoppingCart.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Core.Entities;
+
+public class ShoppingCart
+{
+    public string Id { get; set; }
+    public List<CartItem> Items { get; set; }
+}
